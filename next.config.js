8f464bf5db82c69356537817d64d81248363e7e6
Nic Last <niclast7611@gmail.com>
2022-08-29 14:50:16 -0700

    used graphql to make query,see products in console

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   env: {
     SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
-    SHOPIFY_STOREFRONT_ACCESSTOKEN: process.env.
+    SHOPIFY_STOREFRONT_ACCESSTOKEN: process.env.SHOPIFY_STOREFRONT_ACCESSTOKEN
   },
 
   images: {
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
+import { getProductsInCollection } from "../lib/shopify"
 
-export default function Home() {
+export default function Home({ products }) {
+
+  console.log(products)
   return (
     <div className='text-4xl'>
       <h1>Hello World</h1>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const products = await getProductsInCollection()
+
+  return {
+    props: { products },
+  }
 }
