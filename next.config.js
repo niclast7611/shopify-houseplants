1272e8931b0e15f0edf44b7ae462962a0d36e93f
Nic Last <niclast7611@gmail.com>
2022-08-30 17:09:06 -0700

    got multiple images to show using swiper.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
   env: {
     SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,9 @@ import '../styles/globals.css'
 import Layout from '../components/Layout'
 import ShopProvider from '../context/shopContext'
 import { useRouter } from 'next/router'
-
+import 'swiper/swiper.scss'
+import 'swiper/components/navigation/navigation.scss'
+import 'swiper/components/pagination/pagination.scss'
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
   return (
