6f577fb4bf79b3eaaf9cd49df2c4e697978ec5ff
Nic Last <niclast7611@gmail.com>
2022-08-30 18:25:38 -0700

    added styled hero

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import { getProductsInCollection } from "../lib/shopify"
 import ProductList from "../components/ProductList"
-
+import Hero from "../components/Hero"
 export default function Home({ products }) {
 
   // console.log(products)
   return (
-    <div className='text-4xl'>
+    <div className=''>
+      <Hero />
       <ProductList products={products} />
     </div>
   )
