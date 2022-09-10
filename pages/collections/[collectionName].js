9bf3e14d3ba1eac9829da67fa17c2a83c3077553
niclast7611 <niclast7611@gmail.com>
2022-09-10 14:58:16 -0700

    hello world shows up on dynamic collection page

--- pages/collections/[collectionName].js
+++ pages/collections/[collectionName].js
@@ -1,12 +1,44 @@
-import ProductList from '../../components/ProductList'
+import {  getProduct, getAllCollections } from "../../lib/shopify"
 
-
-export default function CollectionPage({ products }) {
-    const { collectionName } = 'small-plants'
-    const smProducts = products.filter(product => product.collection === collectionName)
+export default function CollectionPage() {
+    // const { collectionName } = 'small-plants'
+    // const smProducts = products.filter(product => product.collection === collectionName)
     // const products = allSmallPlants
-    // console.log('products in collection', products)
     return (
-        <ProductList products={smProducts} />
+        <div>
+            hello world 
+        </div>
     );
+}
+
+export async function getStaticPaths() {
+    const collections = await getAllCollections()
+
+//pre-renders collection paths for each collection name and returns an array of objects with each collections handle in a string
+// Pre-rendered at build time
+    const paths = collections.map(item => {
+        const collectionName = String(item.node.handle)
+        
+        return {
+            params: { collectionName }
+        }
+    })
+
+    console.log(paths)
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+  // This function will run only at build time.
+export async function getStaticProps({ params }) {
+    const product = await getProduct(params.product)
+
+    return {
+        props: {
+            product
+        }
+    }
+
 }
