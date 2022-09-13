2713dee5dbe4207b31a650698683dbc3968501ed
niclast7611 <niclast7611@gmail.com>
2022-09-12 18:39:21 -0700

    disabled remove button unless equal to one

--- components/MiniCart.js
+++ components/MiniCart.js
@@ -23,7 +23,7 @@ export default function MiniCart({ cart }) {
 
         console.log('number', number)
     })
-
+console.log(cart)
     return (
         <Transition.Root show={cartOpen} as={Fragment}>
             <Dialog
@@ -80,7 +80,7 @@ export default function MiniCart({ cart }) {
                                                     {
                                                         cart.length > 0 ?
                                                             <ul role="list" className="-my-6 divide-y divide-white">
-                                                                {cart.map((product) => (
+                                                                {cart.map((product, index) => (
                                                                     <li key={product.id} className="flex py-6">
                                                                         <div className=" relative h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-white">
                                                                             <Image
@@ -108,23 +108,26 @@ export default function MiniCart({ cart }) {
                                                                                 <div className="text-[#fcf9f3]">
                                                                                     <div className="flex flex-wrap flex-col max-h-full content-center">
                                                                                         <div className="flex items-center justify-center">
+                                                                                            {
+                                                                                                index === index ?
+                                                                                            <>
                                                                                             <button
-                                                                                                className="px-2 w-8 h-8 appearance-none bg-none text-xl outline-none border-2 border-solid border-transparent text-[#224229] pb-4 cursor-pointer bg-[#fcf9f3] rounded-full transition-all hover:border-2 hover:border-solid  hover:border-[#224229]/[0.5] focus:outline-2"
-                                                                                                aria-label="Decrement value"
-                                                                                                onClick={() => number != 1 ? setNumber(number - 1) : null}
-                                                                                            >
-                                                                                                -
-                                                                                            </button>
-                                                                                            <span className="text-3xl pl-5 pr-5  mt-1 text-[#fcf9f3]">
-                                                                                                {number}
-                                                                                            </span>
-                                                                                            <button
-                                                                                                className="px-2 w-8 h-8 appearance-none bg-none text-xl outline-none border-2 border-solid border-transparent text-[#224229] pb-4 cursor-pointer bg-[#fcf9f3] rounded-full transition-all hover:border-2 hover:border-solid  hover:border-[#224229]/[0.5] focus:outline-2"
-                                                                                                aria-label="Increment value"
-                                                                                                onClick={() => setNumber(number + 1)}
-                                                                                            >
-                                                                                                +
-                                                                                            </button>
+                                                                                                        className="px-2 w-8 h-8 appearance-none bg-none text-xl outline-none border-2 border-solid border-transparent text-[#224229] pb-4 cursor-pointer bg-[#fcf9f3] rounded-full transition-all hover:border-2 hover:border-solid  hover:border-[#224229]/[0.5] focus:outline-2"
+                                                                                                        aria-label="Decrement value"
+                                                                                                        onClick={() => number != 1 ? setNumber(number - 1) : null}
+                                                                                                    >
+                                                                                                        -
+                                                                                                    </button><span className="text-3xl pl-5 pr-5  mt-1 text-[#fcf9f3]">
+                                                                                                            {number}
+                                                                                                        </span><button
+                                                                                                            className="px-2 w-8 h-8 appearance-none bg-none text-xl outline-none border-2 border-solid border-transparent text-[#224229] pb-4 cursor-pointer bg-[#fcf9f3] rounded-full transition-all hover:border-2 hover:border-solid  hover:border-[#224229]/[0.5] focus:outline-2"
+                                                                                                            aria-label="Increment value"
+                                                                                                            onClick={() => setNumber(number + 1)}
+                                                                                                        >
+                                                                                                            +
+                                                                                                        </button>
+                                                                                                        </> : null
+                                                                                            }
                                                                                         </div>
                                                                                     </div>
                                                                                 </div>
@@ -132,8 +135,9 @@ export default function MiniCart({ cart }) {
                                                                                 <div className="flex">
                                                                                     <button
                                                                                         type="button"
-                                                                                        className="font-medium text-[#fcf9f3] hover:text-white hover:underline"
+                                                                                        className="first-line:font-medium text-[#fcf9f3] hover:text-white hover:underline disabled:opacity-10 disabled:hover:no-underline" 
                                                                                         onClick={() => removeCartItem(product.id)}
+                                                                                        disabled={number > 1}
                                                                                     >
                                                                                         Remove
                                                                                     </button>
