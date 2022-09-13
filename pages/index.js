b3f8d97295cfed3df2d7cc64a2cc6588e9443233
niclast7611 <niclast7611@gmail.com>
2022-09-12 17:43:30 -0700

    simple navigation to collections

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,21 @@
-import { getProductsInCollection, getProductsForHomePage } from "../lib/shopify"
+import { getAllCollections, getProductsForHomePage } from "../lib/shopify"
 import ProductList from "../components/ProductList"
 import Hero from "../components/Hero"
-export default function Home({ homePageProducts }) {
+export default function Home({ homePageProducts, collectionNames }) {
 
   // console.log(homePageProducts.length)
   return (
     <div className='bg-[#fcf9f3]'>
-      <Hero />
+      <Hero collectionNames={collectionNames}/>
       <ProductList homePageProducts={homePageProducts} />
     </div>
   )
 }
 
 export async function getStaticProps() {
-  const products = await getProductsInCollection()
   const homePageProducts = await getProductsForHomePage()
-
+  const collectionNames = await getAllCollections()
   return {
-    props: { products, homePageProducts },
+    props: { homePageProducts, collectionNames },
   }
 }
