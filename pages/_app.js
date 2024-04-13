561c0bfdb48b5385283f42e6b0e266397067b963
Nic Last <104329673+niclast7611@users.noreply.github.com>
2024-04-13 12:30:48 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,25 @@ import { useRouter } from 'next/router'
 import 'swiper/swiper.scss'
 import 'swiper/components/navigation/navigation.scss'
 import 'swiper/components/pagination/pagination.scss'
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
+  
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://ripe.chat/islands/client-chat-button.island.umd.js';
+    script.setAttribute('chatbotId', '112');
+    script.setAttribute('domain', 'https://shopify-houseplants.vercel.app/');
+    document.head.appendChild(script);
+
+    return () => {
+      // Cleanup the script when component unmounts
+      document.head.removeChild(script);
+    };
+  }, []);
+  
   return (
     // cart
     <ShopProvider>
