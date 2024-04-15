3655bb5f0c544f42d8ff80a86edc3eb6f13c12bb
Nic Last <nic@ripemetrics.com>
2024-04-15 10:33:29 -0700

    Refactor ChatScript.js and _app.js to improve code structure and remove unused imports

--- components/ChatScript.js
+++ components/ChatScript.js
@@ -1,26 +1,30 @@
-// components/ChatScript.js
-import Script from "next/script";
-import React from "react";
+import { useEffect } from "react";
 
 const ChatbotScript = ({ chatbotId, domain, env, islandType }) => {
-  return (
-    <Script id="chatbot-script" strategy="afterInteractive">
-      {`
-        (function() {
-            var chatButton = document.createElement('client-chat-button-island');
-            document.body.insertBefore(chatButton, document.body.firstChild);
-            var script = document.createElement('script');
-            script.async = true;
-            script.src = 'https://ripe.chat/islands/client-chat-button.island.umd.js';
-            script.setAttribute('chatbotId', "${chatbotId}");
-            script.setAttribute('domain', "${domain}");
-            script.setAttribute('env', "${env}");
-            script.setAttribute('islandType', "${islandType}");
-            document.head.appendChild(script);
-          })();
-      `}
-    </Script>
-  );
+  console.log("ChatbotScript", chatbotId, domain, env, islandType);
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://ripe.chat/islands/client-chat-button.island.umd.js";
+    script.async = true;
+    script.setAttribute("chatbotId", chatbotId);
+    script.setAttribute("domain", domain);
+    script.setAttribute("env", env);
+    script.setAttribute("islandType", islandType);
+    document.head.appendChild(script);
+
+    const chatButtonElement = document.createElement(
+      "client-chat-button-island"
+    );
+    document.body.appendChild(chatButtonElement);
+
+    return () => {
+      // Cleanup if necessary
+      document.head.removeChild(script);
+      document.body.removeChild(chatButtonElement);
+    };
+  }, [chatbotId, domain, islandType, env]);
+
+  return null;
 };
 
 export default ChatbotScript;
--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,15 @@
-import '../styles/globals.css'
-import Layout from '../components/Layout'
-import ShopProvider from '../context/shopContext'
-import { useRouter } from 'next/router'
-import 'swiper/swiper.scss'
-import 'swiper/components/navigation/navigation.scss'
-import 'swiper/components/pagination/pagination.scss'
-import { useEffect } from 'react';
+import "../styles/globals.css";
+import Layout from "../components/Layout";
+import ShopProvider from "../context/shopContext";
+import { useRouter } from "next/router";
+import "swiper/swiper.scss";
+import "swiper/components/navigation/navigation.scss";
+import "swiper/components/pagination/pagination.scss";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter()
-  
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.async = true;
-    script.src = 'https://ripe.chat/islands/client-chat-button.island.umd.js';
-    script.setAttribute('chatbotId', '112');
-    script.setAttribute('domain', 'https://shopify-houseplants.vercel.app/');
-    document.head.appendChild(script);
+  const router = useRouter();
 
-    return () => {
-      // Cleanup the script when component unmounts
-      document.head.removeChild(script);
-    };
-  }, []);
-  
   return (
     // cart
     <ShopProvider>
@@ -33,7 +19,7 @@ function MyApp({ Component, pageProps }) {
         <Component {...pageProps} key={router.asPath} />
       </Layout>
     </ShopProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
